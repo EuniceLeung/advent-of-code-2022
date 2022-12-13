e64207d179b49e64fc6750bb301dc5e601d7ea4f
efleung2 <efleung2@illinois.edu>
2022-12-13 14:39:16 -0600

    Refactor Day 8.

--- day8/day8.py
+++ day8/day8.py
@@ -7,17 +7,23 @@ def count_visible_trees(input_file):
 
         count = 2 * width + 2 * height - 4
 
+        tops = []
+        bottoms = []
+        for i in range(1, width - 1, 1):
+            tops.append(list(lines[0][i]))
+            bottoms.append(list(lines[j][i] for j in range(2, height, 1)))
+
         for h in range(1, height - 1, 1):
             line = lines[h]
             for i in range(1, width - 1, 1):
                 # visible if greater than max of left, right, top, or bottom nums
                 if line[i] > max(line[:i]) or line[i] > max(line[i + 1:]):
                     count += 1
-                else:
-                    top = list(lines[j][i] for j in range(h))
-                    bottom = list(lines[j][i] for j in range(h + 1, height, 1))
-                    if line[i] > max(top) or line[i] > max(bottom):
-                        count += 1
+                elif line[i] > max(tops[i-1]) or line[i] > max(bottoms[i-1]):
+                    count += 1
+
+                tops[i-1].append(line[i])
+                bottoms[i-1] = bottoms[i-1][1:]
 
         return count
 
@@ -31,18 +37,22 @@ def get_highest_scenic_score(input_file):
         width = len(lines[0])
         height = len(lines)
 
+        tops = []
+        bottoms = []
+        for i in range(1, width - 1, 1):
+            tops.append(list(lines[0][i]))
+            bottoms.append(list(lines[j][i] for j in range(2, height, 1)))
+
         for h in range(1, height - 1, 1):
             line = lines[h]
             for i in range(1, width - 1, 1):
-                elem = line[i]
-
                 left_score = 0
                 right_score = 0
                 top_score = 0
                 bottom_score = 0
                 # visible if greater than max of left, right, top, or bottom nums
-                top = list(lines[j][i] for j in range(h))
-                bottom = list(lines[j][i] for j in range(h + 1, height, 1))
+                top = tops[i-1]
+                bottom = bottoms[i-1]
 
                 if line[i] > max(line[:i]):
                     # left
@@ -92,6 +102,9 @@ def get_highest_scenic_score(input_file):
 
                 score = left_score * right_score * top_score * bottom_score
                 max_score = max(score, max_score)
+
+                tops[i - 1].append(line[i])
+                bottoms[i - 1] = bottoms[i - 1][1:]
 
     return max_score
 
