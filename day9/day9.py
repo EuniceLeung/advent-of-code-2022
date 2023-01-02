ab7973a9bcc5564a21163c246cb1ec914f5ccea0
efleung2 <efleung2@illinois.edu>
2023-01-02 20:13:38 +0800

    Complete day 9 part 2.

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,9 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 def count_tail_positions(input_file):
     tail_pos_set = set()
     tail_pos_set.add((0, 0))  # use (0,0) to represent start
@@ -62,7 +68,63 @@ def count_tail_positions(input_file):
     return len(tail_pos_set)
 
 
+def count_tail_positions2(input_file):
+    head = Point(0, 0)
+    tail_pos_set = set()
+    tail_pos_set.add((0, 0))  # use (0,0) to represent start
+
+    n = 9
+    tail = []
+    for i in range(n):
+        tail.append(Point(0, 0))
+
+    with open(input_file) as f:
+        lines = f.read().splitlines()
+
+        for line in lines:
+            direction, moves = line.split()
+            moves = int(moves)
+
+            for i in range(moves):
+                move_head(head, direction)
+                prev_tail = head
+                for j in range(n):
+                    move_tail(prev_tail, tail[j])
+                    prev_tail = tail[j]
+                tail_pos_set.add((prev_tail.x, prev_tail.y))
+
+    return len(tail_pos_set)
+
+
+def move_head(head, direction):
+    match direction:
+        case 'L':
+            # -x
+            head.x -= 1
+        case 'R':
+            # +x
+            head.x += 1
+        case 'U':
+            # -y
+            head.y += 1
+        case 'D':
+            # +y
+            head.y -= 1
+
+
+def move_tail(head, tail):
+    if abs(head.x - tail.x) > 1 or abs(head.y - tail.y) > 1:
+        if head.x > tail.x:
+            tail.x += 1
+        elif head.x < tail.x:
+            tail.x -= 1
+        if head.y > tail.y:
+            tail.y += 1
+        elif head.y < tail.y:
+            tail.y -= 1
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    result = count_tail_positions('day9data.txt')
+    result = count_tail_positions2('day9data.txt')
     print(result)
